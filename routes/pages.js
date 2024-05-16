beb8e3aad1f1f133fb82edc15282c2f29e9e42fc
Lutik06 <ppolakova240@gmail.com>
2024-05-16 20:29:58 +0300

    /

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,7 @@
 const pagesRouter = require("express").Router();
 const { sendDashboard } = require("../controllers/auth.js");
 const { sendIndex } = require("../controllers/auth.js");
+pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard); 
 
 pagesRouter.get("/", sendIndex);
 pagesRouter.get("/admin/**", sendDashboard);
