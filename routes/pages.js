958b7fdddfcd84270d7022e62e8b760c6c6d6b56
Lutik06 <ppolakova240@gmail.com>
2024-05-19 19:45:21 +0300

    чуток добавила

--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,10 @@
 const pagesRouter = require("express").Router();
 const { sendDashboard } = require("../controllers/auth.js");
 const { sendIndex } = require("../controllers/auth.js");
+const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard); 
 
 pagesRouter.get("/", sendIndex);
-pagesRouter.get("/admin/**", sendDashboard);
+pagesRouter.get("/admin/**", checkAuth, checkCookiesJWT, sendDashboard);
 
 module.exports = pagesRouter;
